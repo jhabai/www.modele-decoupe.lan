f2b796f4fade1d7aee3a1d6bb589cd9758de8d42
LeG3nDz <contact@stephane-richin.fr>
2015-06-09 11:05:43 +0200

    Add credits module

--- includes/reveal-modal/rm-credits.php
+++ includes/reveal-modal/rm-credits.php
@@ -1,23 +1,19 @@
 <h1>Crédits</h1>
 
-<div class="columns large-9 centered">
+<div class="columns small-2 small-centered medium-uncentered medium-2 medium-offset-2 medium-collapse text-center">
+    <a href="http://www.ice-dev.com/" target="_blank" class="credits-logo">
+        <img src="/images/logo/logo-ice-development.svg" alt="Logo Ice Development" width="100%">
+    </a>
+</div>
 
-    <div class="columns large-5 logo-ice text-center">
-        <a href="http://www.ice-dev.com/" target="_blank">
-            <img src="/images/logo/logo-ice-development.svg" alt="Logo Ice Development">
-        </a>
-    </div>
-
-    <div class="columns large-6 large-offset-1 text-center">
-        <ul class="ice-dev">
-            <li><a href="http://www.ice-dev.com/creation-site-web/concevoir.html" target="_blank" rel="nofollow">Conception</a></li>
-            <li><a href="http://www.ice-dev.com/" target="_blank" rel="nofollow">Développement</a></li>
-            <li><a href="http://www.ice-dev.com/hebergement/les-services.html" target="_blank" rel="nofollow">Hébergement</a></li>
-            <li><a href="http://www.ice-dev.com/referencement/referencement-de-site.html" target="_blank" rel="nofollow">Référencement</a></li>
-        </ul>
-        <a href="http://www.ice-dev.com/" class="responsive icon-responsive" target="_blank"><span>www.ice-dev.com</span></a>
-    </div>
-
+<div class="columns small-2 small-centered medium-uncentered medium-2 medium-offset-1 text-center">
+    <ul class="no-bullet credits-ice-dev">
+        <li><a href="http://www.ice-dev.com/creation-site-web/concevoir.html" target="_blank" rel="nofollow">Conception</a></li>
+        <li><a href="http://www.ice-dev.com/" target="_blank" rel="nofollow">Développement</a></li>
+        <li><a href="http://www.ice-dev.com/hebergement/les-services.html" target="_blank" rel="nofollow">Hébergement</a></li>
+        <li><a href="http://www.ice-dev.com/referencement/referencement-de-site.html" target="_blank" rel="nofollow">Référencement</a></li>
+    </ul>
+    <a href="http://www.ice-dev.com/" class="icon-responsive-2" target="_blank"><span>www.ice-dev.com</span></a>
 </div>
 
 <a class="close-reveal-modal" aria-label="Close">&#215;</a>
