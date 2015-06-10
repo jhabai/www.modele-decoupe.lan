572e0b4ee148cbb810db0604869cb27eaffdcdef
LeG3nDz <contact@stephane-richin.fr>
2015-06-10 16:48:41 +0200

    Edit checkbox style

--- includes/grid.php
+++ includes/grid.php
@@ -1,7 +1,6 @@
 <button type="button" class="right button-show-grid" id="button-show-grid">V</button>
-
 <div class="row is-hidden" id="grid">
-    <div class="grid">
+    <div class="grid block-grid">
         <div class="columns small-1 medium-1 large-1"></div>
         <div class="columns small-1 medium-1 large-1"></div>
         <div class="columns small-1 medium-1 large-1"></div>
